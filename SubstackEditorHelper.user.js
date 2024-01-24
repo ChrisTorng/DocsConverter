5a385bc25b0947195d3fb17b8429dbb3afa9b0c9
ChrisTorng <ChrisTorng@hotmail.com>
2024-01-24 14:26:16 +0800

    Update SubstackEditorHelper.user.js.

--- SubstackEditorHelper.user.js
+++ SubstackEditorHelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Substack Editor Helper
-// @version      2024-01-24_0.1.0
+// @version      2024-01-24_0.1.1
 // @description  Make Substack Editor easier to import
 // @homepage     https://github.com/ChrisTorng/DocsConverter/
 // @downloadURL  https://github.com/ChrisTorng/DocsConverter/raw/main/SubstackEditorHelper.user.js
@@ -17,24 +17,22 @@
     document.addEventListener('paste', function(e) {
         e.preventDefault();
         const html = (e.clipboardData || window.clipboardData).getData('text/html');
-        if(html){
-            // document.execCommand('insertHTML', false, html);
-            const editor = document.querySelector('[data-testid="editor"]');
-            console.log(editor.innerHTML);
-
-            // Dealing with footnote links, not working now
-            var links = editor.querySelectorAll('a[target="_blank"]');
-            links.forEach(function(link) {
-                link.removeAttribute('target');
-                link.innerHTML += "(replaced)";
-            });
-
-            console.log(editor.innerHTML);
+        if (!html) {
+            console.log('No HTML data, skipping');
+            return;
         }
-        else {
-            const text = (e.clipboardData || window.clipboardData).getData('text');
-            console.log(text);
-            // document.execCommand('insertText', false, text);
-        }
+
+        // document.execCommand('insertHTML', false, html);
+        const editor = document.querySelector('[data-testid="editor"]');
+        console.log(`Originally: ${editor.innerHTML}`);
+
+        // Dealing with footnote links, not working now
+        var links = editor.querySelectorAll('a[target="_blank"]');
+        links.forEach(function(link) {
+            link.removeAttribute('target');
+            link.innerHTML += "(replaced)";
+        });
+
+        console.log(`Processed: ${editor.innerHTML}`);
     });
 })();
