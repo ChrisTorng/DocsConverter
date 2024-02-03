37df3f3fccd929f1d48d31e0d253010eeeb37e72
ChrisTorng <ChrisTorng@hotmail.com>
2024-02-04 00:03:30 +0800

    SubstackEditorHelper.user.js use array replacements, skip none editor.

--- SubstackEditorHelper.user.js
+++ SubstackEditorHelper.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Substack Editor Helper
-// @version      2024-01-24_0.1.1
+// @version      2024-02-03_0.1.2
 // @description  Make Substack Editor easier to import
 // @homepage     https://github.com/ChrisTorng/DocsConverter/
 // @downloadURL  https://github.com/ChrisTorng/DocsConverter/raw/main/SubstackEditorHelper.user.js
+// @updateURL    https://github.com/ChrisTorng/DocsConverter/raw/main/SubstackEditorHelper.user.js
 // @author       ChrisTorng
 // @match        https://*.substack.com/publish/post/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
@@ -13,25 +14,46 @@
 (function() {
     'use strict';
 
-    const subscribeNowRegEx = /<p>\r?\n?\[\[SUBSCRIBE NOW\]\]\r?\n?<\/p>\r?\n?/g;
-    const subscribeNowHtml = `<p class="button-wrapper"
-data-attrs="{&quot;url&quot;:&quot;%%checkout_url%%&quot;,&quot;text&quot;:&quot;Subscribe now&quot;,&quot;action&quot;:null,&quot;class&quot;:null}"
-data-component-name="ButtonCreateButton"><a class="button primary" href="%%checkout_url%%"><span>Subscribe now</span></a></p>`;
+    const replacements = [
+        // Subscribe now button
+        { regex: /<p>\r?\n?\[\[SUBSCRIBE NOW\]\]\r?\n?<\/p>\r?\n?/g,
+          replacement: `<p class="button-wrapper" data-attrs="{&quot;url&quot;:&quot;%%checkout_url%%&quot;,&quot;text&quot;:&quot;Subscribe now&quot;,&quot;action&quot;:null,&quot;class&quot;:null}" data-component-name="ButtonCreateButton"><a class="button primary" href="%%checkout_url%%"><span>Subscribe now</span></a></p>` },
+        // Footnotes from
+        { regex: /<a target="_blank" rel="footnote" href="https:\/\/[^\/]*\/[a-zA-Z]*#fn(\d+)"><sup>(\d+)<\/sup><\/a>/g,
+          replacement: `<a class="footnote-anchor" data-component-name="FootnoteAnchorToDOM" id="footnote-anchor-$1" href="#footnote-$1" target="_self">$1</a>` },
+        // Footnotes section start
+        { regex: /<h2>Notes<\/h2><hr contenteditable="false"><ol>(.*)<\/ol>/g,
+          replacement: '$1' },
+        // Footnotes to
+        { regex: /<li><p>([^<]*)<a target="_blank" rel="noopener noreferrer nofollow" href="https:\/\/[^\/]*\/[a-zA-Z]*#fnref(\d+)">↩<\/a><\/p><\/li>/g,
+          replacement: `<div class="footnote" data-component-name="FootnoteToDOM"><a id="footnote-$2" href="#footnote-anchor-$2" class="footnote-number" contenteditable="false" target="_self">$2</a><div class="footnote-content"><p>$1</p></div></div>` },
+    ];
 
     // This event got fired after pasted into the editor and converted by Substack
     document.addEventListener('paste', function(e) {
-        e.preventDefault();
-        const html = (e.clipboardData || window.clipboardData).getData('text/html');
-        if (!html) {
-            console.log('No HTML data, skipping');
+        if (e.target.tagName !== 'BR') {
+            console.log('Not in the editor');
             return;
         }
 
+        let html = (e.clipboardData || window.clipboardData).getData('text/html');
+        if (!html) {
+            console.log('No HTML data');
+            return;
+        }
+
+        e.preventDefault();
+
+        console.log(`Pasted: ${html}`);
+
         // document.execCommand('insertHTML', false, html);
         const editor = document.querySelector('[data-testid="editor"]');
-        console.log(`Originally: ${editor.innerHTML}`);
+        console.log(`editor before: ${editor.innerHTML}`);
 
-        editor.innerHTML = editor.innerHTML.replace(subscribeNowRegEx, subscribeNowHtml);
+        // Replace all replacements
+        replacements.forEach(function(replacement) {
+            editor.innerHTML = editor.innerHTML.replace(replacement.regex, replacement.replacement);
+        });
 
         // Dealing with footnote links, not working now
         // var links = editor.querySelectorAll('a[target="_blank"]');
@@ -40,6 +62,6 @@ data-component-name="ButtonCreateButton"><a class="button primary" href="%%check
         //     link.innerHTML += "(replaced)";
         // });
 
-        console.log(`Processed: ${editor.innerHTML}`);
+        console.log(`editor after: ${editor.innerHTML}`);
     });
 })();
